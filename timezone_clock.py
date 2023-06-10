3156bf0ca9d7146789240e0f0dae7a4f98b9b590
atchi <atchisonrb@gmail.com>
2023-06-10 16:06:27 -0500

    Streamline timezone and DST handling, remove unneeded imports, improve backend variable names

--- timezone_clock.py
+++ timezone_clock.py
@@ -1,8 +1,6 @@
 import PySimpleGUI as PySG
 from datetime import datetime as dt2
-import datetime as dt
 import pytz
-import calendar as cal
 
 
 PySG.theme("DarkBlue13")
@@ -56,65 +54,11 @@ while True:
             break
 
     current_utc_datetime = dt2.now(pytz.utc)
+    current_us_ct_datetime = dt2.now(pytz.utc).astimezone(pytz.timezone('US/Central'))
+    current_us_et_datetime = dt2.now(pytz.utc).astimezone(pytz.timezone('America/New_York'))
 
-    current_utc_date = current_utc_datetime.date()
-    current_utc_year = current_utc_datetime.year
-    current_utc_hour = current_utc_datetime.hour
-    current_utc_minute = current_utc_datetime.minute
-
-    utc_cal = cal.Calendar(firstweekday=cal.SUNDAY)
-    mar_cal = utc_cal.monthdatescalendar(current_utc_year, 3)
-    nov_cal = utc_cal.monthdatescalendar(current_utc_year, 11)
-
-    mar_second_sunday = [day for week in mar_cal for day in week if
-                         day.weekday() == cal.SUNDAY and
-                         day.month == 3][1]
-    nov_first_sunday = [day for week in nov_cal for day in week if
-                        day.weekday() == cal.SUNDAY and
-                        day.month == 11][0]
-
-    if current_utc_date < mar_second_sunday:
-        ct_utc_offset = -6
-        et_utc_offset = -5
-    elif current_utc_date == mar_second_sunday:
-        if current_utc_hour < 7:
-            ct_utc_offset = -6
-            et_utc_offset = -5
-        if 7 <= current_utc_hour < 8:
-            ct_utc_offset = -6
-            et_utc_offset = -4
-        else:
-            ct_utc_offset = -5
-            et_utc_offset = -4
-    elif mar_second_sunday < current_utc_date < nov_first_sunday:
-        ct_utc_offset = -5
-        et_utc_offset = -4
-    elif current_utc_date == nov_first_sunday:
-        if current_utc_hour < 6:
-            ct_utc_offset = -5
-            et_utc_offset = -4
-        if 6 <= current_utc_hour < 7:
-            ct_utc_offset = -5
-            et_utc_offset = -5
-        else:
-            ct_utc_offset = -6
-            et_utc_offset = -5
-    else:  # nov_first_sunday < current_utc_date
-        ct_utc_offset = -6
-        et_utc_offset = -5
-
-    if current_utc_hour + ct_utc_offset < 0:
-        current_ct_hour = current_utc_hour + ct_utc_offset + 24
-    else:
-        current_ct_hour = current_utc_hour + ct_utc_offset
-
-    if current_utc_hour + et_utc_offset < 0:
-        current_et_hour = current_utc_hour + et_utc_offset + 24
-    else:
-        current_et_hour = current_utc_hour + et_utc_offset
-
-    window["hour_home"].update(value=str(current_ct_hour))
-    window["hour_work"].update(value=str(current_et_hour))
-    window["minutes"].update(value=f": {str(current_utc_minute)}")
+    window["hour_home"].update(value=str(current_us_ct_datetime.hour))
+    window["hour_work"].update(value=str(current_us_et_datetime.hour))
+    window["minutes"].update(value=f": {('0' + str(current_utc_datetime.minute))[:2]}")
 
 window.close()
