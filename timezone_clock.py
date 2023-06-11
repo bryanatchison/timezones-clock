55813970271beef53635fd92a3b0118be6ea2a1f
atchi <atchisonrb@gmail.com>
2023-06-10 21:30:29 -0500

    Version 2.1: Try new layout.

--- timezone_clock.py
+++ timezone_clock.py
@@ -4,32 +4,82 @@ from datetime import datetime as dt2
 import pytz
 
 # Set up GUI layout.
-PySG.theme("DarkBlue13")
-hour_home = PySG.Text('', key='hour_home', font=('Helvetica', 60))
-hour_work = PySG.Text('', key='hour_work', font=('Helvetica', 60))
-label_home = PySG.Text("Home",  font=('Helvetica', 18))
-minutes = PySG.Text('', key='minutes', font=('Helvetica', 60))
-label_work = PySG.Text("Work", font=('Helvetica', 18))
-exit_button = PySG.Button("Exit", font=('Helvetica', 15))
+hour_home = PySG.Text('', key='hour_home',
+                      font=('Helvetica', 60),
+                      text_color='White',
+                      background_color='Black')
+hour_work = PySG.Text('', key='hour_work',
+                      font=('Helvetica', 60),
+                      text_color='White',
+                      background_color='Black')
+label_home = PySG.Text('home\n',  key='label_home',
+                       font=('Helvetica', 18),
+                       text_color='White',
+                       background_color='Black')
+label_blank_1 = PySG.Text('', key='label_blank_1',
+                          font=('Helvetica', 8),
+                          text_color='White',
+                          background_color='Black')
+label_blank_2 = PySG.Text('', key='label_blank_2',
+                          font=('Helvetica', 9),
+                          text_color='White',
+                          background_color='Black')
+label_work = PySG.Text('\nwork', key='label_work',
+                       font=('Helvetica', 18),
+                       text_color='White',
+                       background_color='Black')
+minutes_digits = PySG.Text('', key='minutes_digits',
+                           font=('Helvetica', 60),
+                           text_color='White',
+                           background_color='Black')
+label_blank_3 = PySG.Text('', key='label_blank_1',
+                          font=('Helvetica', 18),
+                          text_color='White',
+                          background_color='Black')
+label_blank_4 = PySG.Text('', key='label_blank_2',
+                          font=('Helvetica', 18),
+                          text_color='White',
+                          background_color='Black')
+minutes_label = PySG.Text('minutes', key='minutes_label',
+                          font=('Helvetica', 18),
+                          text_color='White',
+                          background_color='Black')
+label_blank_5 = PySG.Text('', key='label_blank_2',
+                          font=('Helvetica', 18),
+                          text_color='White',
+                          background_color='Black')
 
+left_far_column_content = [[hour_home],
+                           [hour_work]]
+left_middle_column_content = [[label_home],
+                              [label_blank_1],
+                              [label_blank_2],
+                              [label_work]]
+right_middle_column_content = [[minutes_digits]]
+right_far_column_content = [[label_blank_3],
+                            [label_blank_4],
+                            [minutes_label],
+                            [label_blank_5]]
 
-left_column_content = [[hour_home],
-                       [hour_work]]
-
-middle_column_content = [[label_home],
-                         [minutes],
-                         [label_work]]
-
-right_column_content = [[exit_button]]
-
-
-left_column = PySG.Column(left_column_content)
-middle_column = PySG.Column(middle_column_content)
-right_column = PySG.Column(right_column_content)
+left_far_column = PySG.Column(left_far_column_content,
+                              background_color='Black',
+                              pad=((2, 1), (2, 2)))
+left_middle_column = PySG.Column(left_middle_column_content,
+                                 background_color='Black',
+                                 pad=((1, 2), (2, 2)))
+right_middle_column = PySG.Column(right_middle_column_content,
+                                  background_color='Black',
+                                  pad=((2, 1), (2, 2)))
+right_far_column = PySG.Column(right_far_column_content,
+                               background_color='Black',
+                               pad=((1, 2), (2, 2)))
 
 # Initialize GUI window.
 window = PySG.Window('Timezone Clock',
-                     layout=[[left_column, middle_column, right_column]])
+                     layout=[[left_far_column, left_middle_column,
+                              right_middle_column, right_far_column]],
+                     background_color='Black',
+                     grab_anywhere=True)
 
 
 # While loop for running clock:
@@ -40,8 +90,6 @@ while True:
     event, values = window.read(timeout=500)
     match event:
         case PySG.WIN_CLOSED:
-            break
-        case "Exit":
             break
 
     # Time and timezone operations:
@@ -54,8 +102,8 @@ while True:
     # Use times to update GUI window
     window['hour_home'].update(value=str(current_us_ct_datetime.hour))
     window['hour_work'].update(value=str(current_us_et_datetime.hour))
-    window['minutes'].update(
-        value=f": {('0' + str(current_utc_datetime.minute))[-2:]}")
+    window['minutes_digits'].update(
+        value=('0' + str(current_utc_datetime.minute))[-2:])
 
 # Close GUI window upon loop break.
 window.close()
